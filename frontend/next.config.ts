955f8fc85b8687a6462f67527515dc62e47a9622
Kauê Silva <kaueksilva12@gmail.com>
2026-05-06 09:58:36 -0300

    fix: mudando saida do next para standalone para rodar no app hosting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  output: 'standalone',
+  /* config options here */
 };
 
 export default nextConfig;
